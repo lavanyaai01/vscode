e835dcf1a3d35f683aa70d44309c371e9d91073d
lavanyam26 <lavanyamai01@gmail.com>
2024-01-03 13:13:48 +0530

    added file

---
+++ program1.c
@@ -0,0 +1,17 @@
+# include <stdio.h>
+int main ()
+{
+    int a,b;
+    a=10;
+    b=20;
+    if (a<b)
+    {
+        printf("a is less than b\n");
+    }
+    else if (a>b)
+    {
+        printf("a is greater than b");
+    }
+    return
+    
+}
