accd6f0be11c754a89a899ca117622efc259b5ef
lavanyam26 <lavanyamai01@gmail.com>
2024-01-05 11:39:16 +0530

    added file

---
+++ main.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include "addeven.c"
+#include "forloop.c"
+#include "ifstatement.c"
+int main()
+{
+    int add();
+    int loop();
+    int positive();
+  {
+     add();
+    loop();
+    positive(); 
+    }
+  return 0;
+}
